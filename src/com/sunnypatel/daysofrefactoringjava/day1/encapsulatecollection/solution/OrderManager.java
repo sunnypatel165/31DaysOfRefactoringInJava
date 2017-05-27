cf916f1fd782b14bab44c6608cebb4498df04bd6
Sunny Patel <sunnypatel165@gmail.com>
2017-05-27 11:02:12 +0530

    Added comments -  Day1

--- src/com/sunnypatel/daysofrefactoringjava/day1/encapsulatecollection/solution/OrderManager.java
+++ src/com/sunnypatel/daysofrefactoringjava/day1/encapsulatecollection/solution/OrderManager.java
@@ -2,7 +2,11 @@ package com.sunnypatel.daysofrefactoringjava.day1.encapsulatecollection.solution
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*
+ * In certain scenarios it is beneficial to not expose a full collection to consumers of a class. 
+Some of these circumstances is when there is additional logic associated with adding/removing items from a collection. 
+Because of this reason, it is a good idea to only expose the collection as something you can iterate over without modifying the collection.
+ */
 public class OrderManager {
 	private List<Order> orderList;
 	private int ordersTotal =0;
