4e38c3109776b1c2df43aa77ad7517d57a46b860
Havan <havanagrawal@gmail.com>
2017-06-07 20:51:58 +0530

    Fix tab to space issue

--- src/com/sunnypatel/daysofrefactoringjava/day11/switchtostrategy/problem/ShippingInfo.java
+++ src/com/sunnypatel/daysofrefactoringjava/day11/switchtostrategy/problem/ShippingInfo.java
@@ -1,29 +1,29 @@
 package com.sunnypatel.daysofrefactoringjava.day11.switchtostrategy.problem;
 
 public class ShippingInfo {
-    public int calculateShippingAmount(State shipToState) {
-        switch (shipToState) {
-        case Alaska:
-            return getAlaskaShippingAmount();
-        case NewYork:
-            return getNewYorkShippingAmount();
-        case Florida:
-            return getFloridaShippingAmount();
-        default:
-            return 0;
-        }
-    }
+	public int calculateShippingAmount(State shipToState) {
+		switch (shipToState) {
+			case Alaska:
+				return getAlaskaShippingAmount();
+			case NewYork:
+				return getNewYorkShippingAmount();
+			case Florida:
+				return getFloridaShippingAmount();
+			default:
+				return 0;
+		}
+	}
 
-    private int getFloridaShippingAmount() {
-        return 15;
-    }
+	private int getFloridaShippingAmount() {
+		return 15;
+	}
 
-    private int getNewYorkShippingAmount() {
-        return 10;
-    }
+	private int getNewYorkShippingAmount() {
+		return 10;
+	}
 
-    private int getAlaskaShippingAmount() {
-        return 3;
-    }
+	private int getAlaskaShippingAmount() {
+		return 3;
+	}
 
 }
