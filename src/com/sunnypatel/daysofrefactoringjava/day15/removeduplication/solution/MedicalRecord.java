65efd18bf0d2d2357904096465f666f3135adb82
Sunny Patel <sunnypatel165@gmail.com>
2017-06-10 08:07:22 +0530

    Day 15 Remove Duplication

--- src/com/sunnypatel/daysofrefactoringjava/day15/removeduplication/solution/MedicalRecord.java
+++ src/com/sunnypatel/daysofrefactoringjava/day15/removeduplication/solution/MedicalRecord.java
@@ -1,31 +1,46 @@
-package com.sunnypatel.daysofrefactoringjava.day16.removeduplication.problem;
+package com.sunnypatel.daysofrefactoringjava.day15.removeduplication.solution;
 
 import java.time.LocalDateTime;
 
+/*
+ * We move the duplicated code to a shared method and voila! 
+ * No more duplication. 
+ * Please enforce this refactoring whenever possible. 
+ * It leads to much fewer bugs because you aren’t copy/pasting the bugs throughout the code.
+*/
+
 public class MedicalRecord {
 	private LocalDateTime dateArchived;
-	
+
 	private boolean isArchived;
-	
-	public void archiveRecord(){
+
+	public void archiveRecord() {
+		switchToArchive();
+	}
+
+	public void closeRecord() {
+		switchToArchive();
+	}
+
+	public void switchToArchive() {
 		this.dateArchived = LocalDateTime.now();
 		this.isArchived = true;
 	}
-	public void closeRecord(){
-		this.dateArchived = LocalDateTime.now();
-		this.isArchived = true;
-	}
-	
+
 	public LocalDateTime getDateArchived() {
 		return dateArchived;
 	}
+
 	public void setDateArchived(LocalDateTime dateArchived) {
 		this.dateArchived = dateArchived;
 	}
+
 	public boolean isArchived() {
 		return isArchived;
 	}
+
 	public void setArchived(boolean isArchived) {
 		this.isArchived = isArchived;
 	}
+
 }
